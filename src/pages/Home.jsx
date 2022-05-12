1f3b9b741b2fbdd32b3908fd4b8fa9334c41b65f
Oscar AGUDELO <oscar.agudelo.pro@gmail.com>
2022-05-12 15:35:59 +0200

    state de l'api sur app et problem css de p ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import Button from "../components_Fathers/Button"
 import "../assets/button.css"
-//import background from "../assets/background.svg"
-// import NameOfComponent from "../components_Fathers/RandomApi";
-// import NameOfComponent from "../components_Fathers/RandomApi";
-// import NameOfComponent from "../components_Fathers/RandomApi";
-// import NameOfComponent from "../components_Fathers/RandomApi";
 
-const Home = () => {
+const Home = ( { apiBdHackathon }) => {
   return (
     <div className="backgroundapp">
       <div className="buttonPlay">
-      <Button/>
+        <div>
+        {apiBdHackathon.map((question, index) => (<p key={index}>{question.Question_Enfant}</p>))}
+        </div>
+      <Button />
       </div>
     </div>
   );
