db12d746c089860b8d6a11e7e67d08beb69da6f4
Valerie <valerie.ielsch@gmail.com>
2022-05-13 00:20:44 +0200

    add div for title description and url, add css for it

--- src/components_Fathers/DisplayAll.jsx
+++ src/components_Fathers/DisplayAll.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 
 const DisplayAll = ({ apiBdHackathon, currentQuestion }) => {
- 
-
   return (
     // modifier class
-    <div>
+    <>
       <div className="bubbleresponse">
         <p>{apiBdHackathon[currentQuestion].Reponse_Parent}</p>
       </div>
-      <div className="bubblequestion"> 
+      <div className="bubblequestion">
         <p>{apiBdHackathon[currentQuestion].Question_Enfant}</p>
       </div>
-      
-      <div className="bubblelinks">
-        <p>{apiBdHackathon[currentQuestion].Titre_du_lien}</p>
-        <p>{apiBdHackathon[currentQuestion].URL}</p>
+
+      <div className="containerUrl">
+        <h2>{apiBdHackathon[currentQuestion].Titre_du_lien}</h2>
+        <p>{apiBdHackathon[currentQuestion].Description}</p>
+        <a href={apiBdHackathon[currentQuestion].URL}>{apiBdHackathon[currentQuestion].URL}</a>
       </div>
-    </div>
+    </>
   );
 };
 
