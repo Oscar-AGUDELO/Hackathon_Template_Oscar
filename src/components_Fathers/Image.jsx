c8b0fa77d0fb6b6b29273d0389e2271eeb9d89e9
guillaumec85 <caliezguillaume@live.fr>
2022-05-13 11:04:20 +0200

    erreur travail sur dev

--- src/components_Fathers/Image.jsx
+++ src/components_Fathers/Image.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import dechetElectro from "../assets/dechetElctro.jpg";
 import dechetVet from "../assets/dechetVet.jpg";
+import jusdefruit from "../assets/jusdefruit.jpg";
+import toxic from "../assets/toxic.jpg";
 
 const Image = ({ apiBdHackathon, currentQuestion }) => {
         if (apiBdHackathon[currentQuestion].ID === "0") {
@@ -9,6 +11,14 @@ const Image = ({ apiBdHackathon, currentQuestion }) => {
         }
         if (apiBdHackathon[currentQuestion].ID === "1") {
           return <img id="dechetElectro" src={dechetElectro} />
+           
+        }
+        if (apiBdHackathon[currentQuestion].ID === "2") {
+          return <img id="jusdefruit" src={jusdefruit} />
+           
+        }
+        if (apiBdHackathon[currentQuestion].ID === "3") {
+          return <img id="toxic" src={toxic} />
            
         }
   console.log(apiBdHackathon[currentQuestion].ID);
